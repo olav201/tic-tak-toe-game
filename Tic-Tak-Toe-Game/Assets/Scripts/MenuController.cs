bb920186eff4971264b3fc6098e8026de8689cca
olav201 <olav201@hotmail.com>
2018-04-19 12:14:00 +0200

    Game assets
    test

---
+++ Tic-Tak-Toe-Game/Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+
+    public void Start()
+    {
+      
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
